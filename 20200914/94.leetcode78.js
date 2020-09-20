ded50aaa8c29332e6319eaa82af7da8790c12576
Btrya <584683599@qq.com>
2020-09-20 13:39:18 +0800

    2020/09/20

---
+++ 20200914/94.leetcode78.js
@@ -0,0 +1,37 @@
+/**
+ * 2020/09/20 每日一题
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 说明：解集不能包含重复的子集。
+ * 示例:
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ *   [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  const res = []
+  const dfs = (index, list) => {
+    if (index === nums.length) {
+      res.push(list.slice())
+      return 
+    }
+    list.push(nums[index])
+    dfs(index + 1, list)
+    list.pop()
+    dfs(index + 1, list)
+  }
+  dfs(0, [])
+  return res
+};
