36a4435040e84858cad1bb951db223d3e65d9cc0
Btrya <584683599@qq.com>
2020-11-04 14:19:55 +0800

    2020/11/4 每日一题

---
+++ 20201102/137.leetcode57.js
@@ -0,0 +1,39 @@
+/**
+ * 2020/11/4 每日一题 57. 插入区间
+ * 给出一个无重叠的 ，按照区间起始端点排序的区间列表。
+ * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
+ * 示例 1：
+ * 
+ * 输入：intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * 输出：[[1,5],[6,9]]
+ * 示例 2：
+ * 
+ * 输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * 输出：[[1,2],[3,10],[12,16]]
+ * 解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。
+ */
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+  const res = []
+  let i = 0
+  const len = intervals.length
+  while (i < len && intervals[i][1] < newInterval[0]) {
+    res.push(intervals[i])
+    i++
+  }
+  while (i < len && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0])
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1])
+    i++
+  }
+  res.push(newInterval)
+  while (i < len) {
+    res.push(intervals[i])
+    i++
+  }
+  return res
+};
