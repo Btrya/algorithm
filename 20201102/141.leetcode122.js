b01b9565e34c5c6b76bd91df2a1ffa61fe22e920
Btrya <584683599@qq.com>
2020-11-08 22:20:26 +0800

    2020/11/8 每日一题

---
+++ 20201102/141.leetcode122.js
@@ -0,0 +1,81 @@
+/**
+ * 2020/11/8 每日一题 122. 买卖股票的最佳时机 II
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 示例 1:
+ * 
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 7
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ *      随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+ * 示例 2:
+ * 
+ * 输入: [1,2,3,4,5]
+ * 输出: 4
+ * 解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ *      注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+ *      因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+ * 示例 3:
+ * 
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ */
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+// 动态规划
+var maxProfit = function(prices) {
+  const n = prices.length
+  let dp0 = 0, dp1 = -prices[0]
+  for (let i = 0; i < n; i++) {
+    let newDp0 = Math.max(dp0, dp1 + prices[i])
+    let newDp1 = Math.max(dp1, dp0 - prices[i])
+    dp0 = newDp0
+    dp1 = newDp1
+  }
+  return dp0
+};
+// 贪心
+var maxProfit = function(prices) {
+  let ans = 0
+  let n = prices.length
+  for (let i = 1; i < n; i++) {
+    ans += Math.max(0, prices[i] - prices[i - 1])
+  }
+  return ans
+};
+
+/**
+ * 拓展 121. 买卖股票的最佳时机
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+ * 注意：你不能在买入股票前卖出股票。
+ * 示例 1:
+ * 
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ * 示例 2:
+ * 
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ */
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  if (!prices) return 0
+  let min = Number.MAX_SAFE_INTEGER, max = 0
+  for (let i = 0, price; i < prices.length; i++) {
+    price = prices[i]
+    min = Math.min(min, price)
+    max = Math.max(max, price - min)
+  }
+  return max
+};
