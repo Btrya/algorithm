60bec65f6ec2ba37bbdf02cd2882b15714755251
Btrya <584683599@qq.com>
2020-11-25 17:32:07 +0800

    2020/11/25 加餐

--- 20201123/158.leetcode1370.js
+++ 20201123/158.leetcode1370.js
@@ -63,3 +63,47 @@ var sortString = function(s) {
   }
   return ret
 };
+
+/**
+ * 加餐 3. 无重复字符的最长子串
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 示例 1:
+ * 
+ * 输入: s = "abcabcbb"
+ * 输出: 3 
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * 
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * 
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 示例 4:
+ * 
+ * 输入: s = ""
+ * 输出: 0
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// 滑动窗口
+var lengthOfLongestSubstring = function(s) {
+  const occ = new Set()
+  const n = s.length
+  let rk = -1, ans = 0
+  for (let i = 0; i < n; i++) {
+    if (i != 0) occ.delete(s.charAt(i - 1))
+    while (rk + 1 < n && !occ.has(s.charAt(rk + 1))) {
+      occ.add(s.charAt(rk + 1))
+      rk++
+    }
+    ans = Math.max(ans, rk - i + 1)
+  }
+  return ans
+};
