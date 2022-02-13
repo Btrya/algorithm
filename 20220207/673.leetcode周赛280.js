254ac9d28d54477752a23371904b24596666c951
Btrya <584683599@qq.com>
2022-02-13 15:41:21 +0800

    2022/02/13 周赛

--- 20220207/673.leetcode周赛280.js
+++ 20220207/673.leetcode周赛280.js
@@ -113,3 +113,146 @@
     return oddCount - odd[0][1] + evenCount - even[0][1]
   }
 };
+
+/**
+ * 6006. 拿出最少数目的魔法豆
+ * 给你一个 正 整数数组 beans ，其中每个整数表示一个袋子里装的魔法豆的数目。
+ * 
+ * 请你从每个袋子中 拿出 一些豆子（也可以 不拿出），使得剩下的 非空 袋子中（即 至少 还有 一颗 魔法豆的袋子）魔法豆的数目 相等 。一旦魔法豆从袋子中取出，你不能将它放到任何其他的袋子中。
+ * 
+ * 请你返回你需要拿出魔法豆的 最少数目。
+ * 
+ *  
+ * 
+ * 示例 1：
+ * 
+ * 输入：beans = [4,1,6,5]
+ * 输出：4
+ * 解释：
+ * - 我们从有 1 个魔法豆的袋子中拿出 1 颗魔法豆。
+ *   剩下袋子中魔法豆的数目为：[4,0,6,5]
+ * - 然后我们从有 6 个魔法豆的袋子中拿出 2 个魔法豆。
+ *   剩下袋子中魔法豆的数目为：[4,0,4,5]
+ * - 然后我们从有 5 个魔法豆的袋子中拿出 1 个魔法豆。
+ *   剩下袋子中魔法豆的数目为：[4,0,4,4]
+ * 总共拿出了 1 + 2 + 1 = 4 个魔法豆，剩下非空袋子中魔法豆的数目相等。
+ * 没有比取出 4 个魔法豆更少的方案。
+ * 示例 2：
+ * 
+ * 输入：beans = [2,10,3,2]
+ * 输出：7
+ * 解释：
+ * - 我们从有 2 个魔法豆的其中一个袋子中拿出 2 个魔法豆。
+ *   剩下袋子中魔法豆的数目为：[0,10,3,2]
+ * - 然后我们从另一个有 2 个魔法豆的袋子中拿出 2 个魔法豆。
+ *   剩下袋子中魔法豆的数目为：[0,10,3,0]
+ * - 然后我们从有 3 个魔法豆的袋子中拿出 3 个魔法豆。
+ *   剩下袋子中魔法豆的数目为：[0,10,0,0]
+ * 总共拿出了 2 + 2 + 3 = 7 个魔法豆，剩下非空袋子中魔法豆的数目相等。
+ * 没有比取出 7 个魔法豆更少的方案。
+ *  
+ * 
+ * 提示：
+ * 
+ * 1 <= beans.length <= 105
+ * 1 <= beans[i] <= 105
+ */
+/**
+ * @param {number[]} beans
+ * @return {number}
+ */
+// 默认最大堆
+const defaultCmp = (x, y) => x < y;
+// 交换元素
+const swap = (arr, i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]]);
+// 堆类，默认最大堆
+class Heap {
+  constructor(cmp = defaultCmp) {
+    this.container = [];
+    this.cmp = cmp;
+  }
+  // 插入
+  insert(data) {
+    const {
+      container,
+      cmp
+    } = this;
+    container.push(data);
+    let index = this.size() - 1;
+    while (index) {
+      let parent = (index - 1) >> 1;
+      if (!cmp(container[index], container[parent])) {
+        return;
+      }
+      swap(container, index, parent);
+      index = parent;
+    }
+  }
+  // 弹出堆顶，并返回
+  pop() {
+    const {
+      container,
+      cmp
+    } = this;
+    if (!this.size()) {
+      return null;
+    }
+
+    swap(container, 0, this.size() - 1);
+    const res = container.pop();
+    const length = this.size();
+    let index = 0,
+      exchange = index * 2 + 1;
+
+    while (exchange < length) {
+      // // 以最大堆的情况来说：如果有右节点，并且右节点的值大于左节点的值
+      let right = index * 2 + 2;
+      if (right < length && cmp(container[right], container[exchange])) {
+        exchange = right;
+      }
+      if (!cmp(container[exchange], container[index])) {
+        break;
+      }
+      swap(container, exchange, index);
+      index = exchange;
+      exchange = index * 2 + 1;
+    }
+
+    return res;
+  }
+  // 获取堆大小
+  size() {
+    return this.container.length;
+  }
+}
+var minimumRemoval = function(beans) {
+  let ans = -1
+  let sums = beans.reduce((total, prev) => total += prev, 0)
+  let len = beans.length
+  let bean = 0
+  let heapBeans = new Heap()
+  for (let i = 0; i < len; ++i) {
+    heapBeans.insert(beans[i])
+  }
+  while (heapBeans.size()) {
+    const num = heapBeans.pop()
+    if (ans === -1 || ans > (sums - (len - bean) * num)) {
+      ans = sums - (len - bean) * num
+    }
+    bean++
+  }
+  return ans
+};
+/**
+ * @param {number[]} beans
+ * @return {number}
+ */
+ var minimumRemoval = function(beans) {
+  const sum = beans.reduce((cur, next) => cur + next, 0)
+  let ans = sum
+  beans.sort((a, b) => a - b)
+  for (let i = 0; i < beans.length; i ++) {
+      ans = Math.min(ans, sum - beans[i] * (beans.length - i))
+  }
+  return ans
+};
