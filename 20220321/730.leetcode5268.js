9ff26c696a59bc201ebec3e5f7441229a35e5163
Btrya <584683599@qq.com>
2022-03-27 14:06:25 +0800

    2022/03/27 每日三题

---
+++ 20220321/730.leetcode5268.js
@@ -0,0 +1,57 @@
+/**
+ * 2022/03/27 每日三题 5268. 找出两数组的不同
+ * 给你两个下标从 0 开始的整数数组 nums1 和 nums2 ，请你返回一个长度为 2 的列表 answer ，其中：
+ * 
+ * answer[0] 是 nums1 中所有 不 存在于 nums2 中的 不同 整数组成的列表。
+ * answer[1] 是 nums2 中所有 不 存在于 nums1 中的 不同 整数组成的列表。
+ * 注意：列表中的整数可以按 任意 顺序返回。
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums1 = [1,2,3], nums2 = [2,4,6]
+ * 输出：[[1,3],[4,6]]
+ * 解释：
+ * 对于 nums1 ，nums1[1] = 2 出现在 nums2 中下标 0 处，然而 nums1[0] = 1 和 nums1[2] = 3 没有出现在 nums2 中。因此，answer[0] = [1,3]。
+ * 对于 nums2 ，nums2[0] = 2 出现在 nums1 中下标 1 处，然而 nums2[1] = 4 和 nums2[2] = 6 没有出现在 nums2 中。因此，answer[1] = [4,6]。
+ * 示例 2：
+ * 
+ * 输入：nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+ * 输出：[[3],[]]
+ * 解释：
+ * 对于 nums1 ，nums1[2] 和 nums1[3] 没有出现在 nums2 中。由于 nums1[2] == nums1[3] ，二者的值只需要在 answer[0] 中出现一次，故 answer[0] = [3]。
+ * nums2 中的每个整数都在 nums1 中出现，因此，answer[1] = [] 。 
+ *  
+ * 
+ * 提示：
+ * 
+ * 1 <= nums1.length, nums2.length <= 1000
+ * -1000 <= nums1[i], nums2[i] <= 1000
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+ var findDifference = function(nums1, nums2) {
+  // 去重
+  nums1 = Array.from(new Set(nums1))
+  nums2 = Array.from(new Set(nums2))
+  // 求 arr1 中所有 不 存在于 arr2 中的 不同 整数组成的列表
+  const findDiffArr = (arr1, arr2) => {
+    let map = {}
+    let res = []
+    // 先记录 arr2 中都有啥
+    for (let i = 0; i < arr2.length; ++i) {
+      map[arr2[i]] = true
+    }
+    // 遍历 arr1 找到 arr2 中没有的
+    for (let i = 0; i < arr1.length; ++i) {
+      if (!map[arr1[i]]) res.push(arr1[i])
+    }
+    return res
+  }
+  return [
+    findDiffArr(nums1, nums2),
+    findDiffArr(nums2, nums1)
+  ]
+};
