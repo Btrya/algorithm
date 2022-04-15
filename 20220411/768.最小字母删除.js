6c16491fd9b10fa58c8ca69801d1416874d72ad0
Btrya <584683599@qq.com>
2022-04-15 17:10:25 +0800

    2022/04/15 群友贡献题

---
+++ 20220411/768.最小字母删除.js
@@ -0,0 +1,40 @@
+/**
+ * 2022/04/15 群友贡献题
+ * 【最小字母删除】
+ * 对于一个仅含有小写字母的字符串，定义一次删除操作：
+ * 选择字符串中最小字典序的字母，若有多个相同字母，则选择最左边那个，将其从字符串中删除
+ * 给定字符串和正整数k，输出进行k次删除操作之后的字符串结果
+ * 
+ * 输入描述
+ * 一个仅含有小写字母的字符串s
+ * k
+ * 
+ * 输出描述
+ * 进行k次删除操作之后的字符串
+ * 
+ * 【示例】
+ * 输入
+ * badabpzib
+ * 3
+ * 
+ * 输出
+ * dbpzib
+ */
+function delMinChar(s, k) {
+  const n = s.length
+  if (n < k) return ''
+  const charArr = Array.from(new Array(26), () => [])
+  let res = ''
+  for (let i = 0; i < n; ++i) {
+    const charCode = s[i].charCodeAt() - 97
+    charArr[charCode].push(i)
+  }
+  // 拍平数组
+  const flatArr = charArr.flat().slice(0, k)
+  for (let i = 0; i < n; ++i) {
+    if (!flatArr.includes(i)) res += s[i]
+  }
+  return res
+}
+
+console.log(delMinChar('badabpzib', 3)) // dbpzib
