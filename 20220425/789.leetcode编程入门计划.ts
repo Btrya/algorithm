48b87160207e3444e3f54d538f8544ecb8858928
Btrya <584683599@qq.com>
2022-04-27 20:11:56 +0800

    2022/04/27 每日一题

---
+++ 20220425/789.leetcode编程入门计划.ts
@@ -0,0 +1,102 @@
+/**
+ * 2022/04/27 编程入门计划
+ * 1603. 设计停车系统
+ * 请你给一个停车场设计一个停车系统。停车场总共有三种不同大小的车位：大，中和小，每种尺寸分别有固定数目的车位。
+ * 
+ * 请你实现 ParkingSystem 类：
+ * 
+ * ParkingSystem(int big, int medium, int small) 初始化 ParkingSystem 类，三个参数分别对应每种停车位的数目。
+ * bool addCar(int carType) 检查是否有 carType 对应的停车位。 carType 有三种类型：大，中，小，分别用数字 1， 2 和 3 表示。一辆车只能停在  carType 对应尺寸的停车位中。如果没有空车位，请返回 false ，否则将该车停入车位并返回 true 。
+ *  
+ * 示例 1：
+ * 
+ * 输入：
+ * ["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+ * [[1, 1, 0], [1], [2], [3], [1]]
+ * 输出：
+ * [null, true, true, false, false]
+ * 
+ * 解释：
+ * ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+ * parkingSystem.addCar(1); // 返回 true ，因为有 1 个空的大车位
+ * parkingSystem.addCar(2); // 返回 true ，因为有 1 个空的中车位
+ * parkingSystem.addCar(3); // 返回 false ，因为没有空的小车位
+ * parkingSystem.addCar(1); // 返回 false ，因为没有空的大车位，唯一一个大车位已经被占据了
+ *  
+ * 
+ * 提示：
+ * 
+ * 0 <= big, medium, small <= 1000
+ * carType 取值为 1， 2 或 3
+ * 最多会调用 addCar 函数 1000 次
+ */
+ class ParkingSystem {
+  private leftPark: number[]
+  constructor(big: number, medium: number, small: number) {
+    this.leftPark = [big, medium, small]
+  }
+
+  addCar(carType: number): boolean {
+    return this.leftPark[carType - 1]-- > 0
+  }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
+
+/**
+ * 303. 区域和检索 - 数组不可变
+ * 给定一个整数数组  nums，处理以下类型的多个查询:
+ * 
+ * 计算索引 left 和 right （包含 left 和 right）之间的 nums 元素的 和 ，其中 left <= right
+ * 实现 NumArray 类：
+ * 
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 中索引 left 和 right 之间的元素的 总和 ，包含 left 和 right 两点（也就是 nums[left] + nums[left + 1] + ... + nums[right] )
+ *  
+ * 
+ * 示例 1：
+ * 
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ * 
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1)) 
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ *  
+ * 
+ * 提示：
+ * 
+ * 1 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ * 0 <= i <= j < nums.length
+ * 最多调用 104 次 sumRange 方法
+ */
+ class NumArray {
+  private prefixSum: number[]
+  constructor(nums: number[]) {
+    const n = nums.length
+    this.prefixSum = new Array(n + 1).fill(0)
+    for (let i = 0; i < n; ++i) {
+      this.prefixSum[i + 1] = nums[i] + this.prefixSum[i]
+    }
+  }
+
+  sumRange(left: number, right: number): number {
+    return this.prefixSum[right + 1] - this.prefixSum[left]
+  }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
