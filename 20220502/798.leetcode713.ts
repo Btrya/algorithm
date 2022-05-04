65eea7a89fb599a1b0e93bfce4a5cfd1fdf18674
Btrya <cbtrya@foxmail.com>
2022-05-05 01:09:48 +0800

    2022/05/05 每日一题

---
+++ 20220502/798.leetcode713.ts
@@ -0,0 +1,35 @@
+/**
+ * 2022/05/05 713. 乘积小于 K 的子数组
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回子数组内所有元素的乘积严格小于 k 的连续子数组的数目。
+ *  
+ * 示例 1：
+ * 
+ * 输入：nums = [10,5,2,6], k = 100
+ * 输出：8
+ * 解释：8 个乘积小于 100 的子数组分别为：[10]、[5]、[2],、[6]、[10,5]、[5,2]、[2,6]、[5,2,6]。
+ * 需要注意的是 [10,5,2] 并不是乘积小于 100 的子数组。
+ * 示例 2：
+ * 
+ * 输入：nums = [1,2,3], k = 0
+ * 输出：0
+ *  
+ * 
+ * 提示: 
+ * 
+ * 1 <= nums.length <= 3 * 104
+ * 1 <= nums[i] <= 1000
+ * 0 <= k <= 106
+ */
+function numSubarrayProductLessThanK(nums: number[], k: number): number {
+  let res = 0, n = nums.length
+  let prod = 1, i = 0
+  for (let j = 0; j < n; ++j) {
+    prod *= nums[j]
+    while (i <= j && prod >= k) {
+      prod /= nums[i]
+      i ++
+    }
+    res += j - i + 1
+  }
+  return res
+};
